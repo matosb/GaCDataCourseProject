ed6807880395469be2da3ca563bf3de05c4402ea
Thomas Bierlaire <thomas.bierlaire@gmail.com>
2018-09-01 15:30:24 +0200

    submission

--- CodeBook.md
+++ CodeBook.md
@@ -1,8 +1,34 @@
 <h1>CodeBook for the Course Project</h1>
 <p>This codebook describes :</p>
-<ul>
- <li>the variables</li>
- <li>the data</li>
- <li>any transformations or work that were performed to clean up the data</li>
+<ol>
+ <li>The data and the variables</li>
+ <li>The variables</li>
+ <li>Any transformations or work that were performed to clean up the data</li>
+</ol>
+
+<h2>The data and the variables</h2>
+<a href="http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones">They were collected from the accelerometers from the Samsung Galaxy S smartphone</a>
+<p>On this dataset we have 10.299 rows and 561 columns</p>
+
+<h2>Any transformations or work that were performed to clean up the data</h2>
+<h3>1. Merges the training and the test sets to create one data set.</h3>
+<ul>Loading training data :
+  <li>../UCI HAR Dataset/train/X_train.txt</li>
+  <li>../UCI HAR Dataset/train/y_train.txt</li>
+  <li>../UCI HAR Dataset/train/subject_train.txt</li>
 </ul>
+<ul>Loading testing data :
+  <li>../UCI HAR Dataset/test/X_test.txt</li>
+  <li>../UCI HAR Dataset/test/y_test.txt</li>
+  <li>../UCI HAR Dataset/test/subject_test.txt</li>
+</ul>
+
+<p>Merging training and testing data</p>
+
+<h3>2. Extracts only the measurements on the mean and standard deviation for each measurement.</h3>
+
+<h3>3. Appropriately labels the data set with descriptive variable names
+and uses descriptive activity names to name the activities in the data set</h3>
+
+<h3>4. From the data set in step 3, creates a second, independent tidy data set with the average of each variable for each activity and each subject.</h3>
 
--- run_analysis.R
+++ run_analysis.R
@@ -1,11 +1,18 @@
 # 1. Merges the training and the test sets to create one data set.
+# Loading training data
+X_train <- read.table("../UCI HAR Dataset/train/X_train.txt")
+y_train <- read.table("../UCI HAR Dataset/train/y_train.txt")
+subject_train <- read.table("../UCI HAR Dataset/train/subject_train.txt")
 
-# Loading X_train.txt and X_test.txt
-training <- read.table("../UCI HAR Dataset/train/X_train.txt")
-testing <- read.table("../UCI HAR Dataset/test/X_test.txt")
+# Loading testing data
+X_test <- read.table("../UCI HAR Dataset/test/X_test.txt")
+y_test <- read.table("../UCI HAR Dataset/test/y_test.txt")
+subject_test <- read.table("../UCI HAR Dataset/test/subject_test.txt")
 
 # Merging training and testing
-data <- rbind(training, testing)
+data <- rbind(X_train, X_test)
+activity <- rbind(y_train, y_test)
+subject <- rbind(subject_train, subject_test)
 
 # 2. Extracts only the measurements on the mean and standard deviation for each measurement.
 # Loading dplyr library for manipulating dataframe 
@@ -15,15 +22,23 @@ features <- read.table("../UCI HAR Dataset/features.txt")
 featuresFiltered <- filter(features, grepl("mean|std", V2))
 dataselected <- subset(data, select=featuresFiltered$V1)
 
-# 3. Uses descriptive activity names to name the activities in the data set
+# 3. Appropriately labels the data set with descriptive variable names 
+# and uses descriptive activity names to name the activities in the data set
+names(dataselected) <- featuresFiltered$V2
+# Adding activity and subject to the dataset
+dataselected$activity <- activity$V1
+dataselected$subject <- subject$V1
+# Use descriptive activity names
+dataselected$activity[dataselected$activity == 1] <- "WALKING"
+dataselected$activity[dataselected$activity == 2] <- "WALKING_UPSTAIRS"
+dataselected$activity[dataselected$activity == 3] <- "WALKING_DOWNSTAIRS"
+dataselected$activity[dataselected$activity == 4] <- "SITTING"
+dataselected$activity[dataselected$activity == 5] <- "STANDING"
+dataselected$activity[dataselected$activity == 6] <- "LAYING"
 
-
-# 4. Appropriately labels the data set with descriptive variable names.
-library(plyr)
-dataselected <- rename(dataselected,c("V1" = "TimeSignalMeanBodyAccelerationX", "V2" = "TimeSignalMeanBodyAccelerationY", "V3" = "TimeSignalMeanBodyAccelerationZ", "V4" = "TimeSignalStdBodyAccelerationX", "V5" = "TimeSignalStdBodyAccelerationY", "V6" = "TimeSignalStdBodyAccelerationZ"))
-dataselected <- rename(dataselected,c("V41" = "TimeSignalMeanGravityAccelerationX", "V42" = "TimeSignalMeanGravityAccelerationY", "V43" = "TimeSignalMeanGravityAccelerationZ", "V44" = "TimeSignalStdGravityAccelerationX", "V45" = "TimeSignalStdGravityAccelerationY", "V46" = "TimeSignalStdGravityAccelerationZ"))
-dataselected <- rename(dataselected,c("V81" = "TimeSignalMeanBodyAccelerationJerkX", "V82" = "TimeSignalMeanBodyAccelerationJerkY", "V83" = "TimeSignalMeanBodyAccelerationJerkZ", "V84" = "TimeSignalStdBodyAccelerationJerkX", "V85" = "TimeSignalStdBodyAccelerationJerkY", "V86" = "TimeSignalStdBodyAccelerationJerkZ"))
-dataselected <- rename(dataselected,c("V121" = "TimeSignalMeanBodyGyroX", "V122" = "TimeSignalMeanBodyGyroY", "V123" = "TimeSignalMeanBodyGyroZ", "V124" = "TimeSignalStdBodyGyroX", "V125" = "TimeSignalStdBodyGyroY", "V126" = "TimeSignalStdBodyGyroZ"))
-dataselected <- rename(dataselected,c("V161" = "TimeSignalMeanBodyGyroJerkX", "V162" = "TimeSignalMeanBodyGyroJerkY", "V163" = "TimeSignalMeanBodyGyroJerkZ", "V164" = "TimeSignalStdBodyGyroJerkX", "V165" = "TimeSignalStdBodyGyroJerkY", "V166" = "TimeSignalStdBodyGyroJerkZ"))
-
-# 5. From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+# 4. From the data set in step 3, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+dataselected$key <- paste(dataselected$activity, dataselected$subject)
+library(data.table)
+dt <- data.table(dataselected)
+colstoavg <- names(dt)[1:79]
+dt.mean <- dt[,lapply(.SD,mean,na.rm=TRUE),by=key,.SDcols=colstoavg]
